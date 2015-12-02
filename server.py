1f2f304d313c417efa1b607da4d87ba2481a45bf
vickydasta <vickydasta@gmail.com>
2015-12-02 20:19:49 +0700

    Update server.py

--- server.py
+++ server.py
@@ -10,17 +10,10 @@ api = Api(app)
 
 data = getdata()
 
-
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
-
-parser = reqparse.RequestParser()
-parser.add_argument('task')
-
 class GetSome(Resource):
     def get(self):
         return data
+        
 # 0.0.0.0/v2
 api.add_resource(GetSome, '/v2')
 
